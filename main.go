40c019546e2f82e518f4d6e18fc2b381b7a268cf
Jean Marc Lambert <jmlambert78@gmail.com>
2020-04-16 21:11:55 +0200

    Update main.go

--- main.go
+++ main.go
@@ -3,25 +3,25 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
+
+	"github.com/itchyny/gojq"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Tekton build on SUSE CaaSP 4.1"
-
-	from := ""
-	if r.URL != nil {
-		from = r.URL.String()
-	}
-	if from != "/favicon.ico" {
-		log.Printf("title: %s\n", title)
-	}
-
-	fmt.Fprintf(w, "Hello from:  "+title+"\n")
-}
-
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	query, err := gojq.Parse(".foo | ..")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	input := map[string]interface{}{"foo": []interface{}{1, 2, 3}}
+	iter := query.Run(input) // or query.RunWithContext
+	for {
+		v, ok := iter.Next()
+		if !ok {
+			break
+		}
+		if err, ok := v.(error); ok {
+			log.Fatalln(err)
+		}
+		fmt.Printf("%#v\n", v)
+	}
 }
-
